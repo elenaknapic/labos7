89e88d45654d1ace6d39b97434544bd98b8bd7d3
Elena Knapić <51436221+elenaknapic@users.noreply.github.com>
2020-05-31 23:34:17 +0200

    Importing project

---
+++ CheckPalindrome/PalindromeCheck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace CheckPalindrome
+{
+    public class PalindromeCheck
+    {
+        public bool Palindrome(string palindromeString)
+        {
+            string first = palindromeString.Substring(0, palindromeString.Length / 2);
+            char[] arr = palindromeString.ToCharArray();
+
+            Array.Reverse(arr);
+
+            string temp = new string(arr);
+            string second = temp.Substring(0, temp.Length / 2);
+
+            return first.Equals(second);
+        }
+
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using CheckPalindrome;
+
+
+namespace CheckPalindrome.Test
+
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        PalindromeCheck palindromeCheck = new PalindromeCheck();
+
+        //Provjera kad je palindrom
+        [TestCase("ana")]
+        [TestCase("madam")]
+        [TestCase("Sir ima miris")]
+        [TestCase("sirimamiris")]
+        [TestCase("ANA")]
+
+        public void TestMethod1(string input)
+        {
+            bool expected = true;
+            bool actual = palindromeCheck.Palindrome(input);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(expected, actual);
+
+        }
+
+
+        //Provjera kada nije palindrom
+        [TestCase("Ovo nije palindrom")]
+        [TestCase("Sto je palindrom")]
+        public void TestMethod2(string input)
+        {
+            bool expected = false;
+            bool actual = palindromeCheck.Palindrome(input);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(expected, actual);
+        }
+
+
+
+        //Provjera kad je empty
+        [TestCase(" ")]
+        [TestCase()]
+        public void TestMethod3(string input)
+        {
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.ThrowsException<ArgumentException>(() => palindromeCheck.Palindrome(input));
+        }
+
+
+    }
+}
